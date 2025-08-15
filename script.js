efd01cce630861ce33e152db29dddbf01d05a324
Mohd Kasif Khan <developer786kasif@gmail.com>
2025-08-15 18:24:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function chnage() {
+	let ptags = document.getElementById("ptag")
+	let htags = documnet.createElement("h1");
+	htags.textContent = ptags.textContent
+	ptags.replaceWith(htags)
+}
